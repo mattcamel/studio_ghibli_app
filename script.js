d2d163dc2a2815693dbc5143bb20acc1de244b85
Matthew Camellato <matthewcamellato@Matthews-MacBook-Air.local>
2019-12-20 07:11:56 -0500

    Completed Ghibli App

---
+++ script.js
@@ -0,0 +1,62 @@
+// var request = new XMLHttpRequest()
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+// request.onload = function(){
+//   var data = JSON.parse(this.response)
+//   if(request.status >= 200 && request.status < 400){
+//     data.forEach(movie => console.log(movie.title))
+//   }else{
+//     const errorMessage = document.createElement('marquee')
+//     errorMessage.textContent = `Gah it's not working!!!`
+//     app.appendChild(errorMessage)
+//   } 
+// }
+// request.send()
+
+const app = document.getElementById('root')
+const logo = document.createElement('img')
+// logo.src = 'https://raw.githubusercontent.com/taniarascia/sandbox/master/ghibli/logo.png'
+logo.src = 'logo.svg'
+
+const message = document.createElement('marquee')
+message.textContent = `S t u d i o  G h i b l i . . . . .`
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+const link_base = 'https://www.studioghibli.com.au/'
+const regexp = /\s/g
+
+app.appendChild(logo)
+app.appendChild(container)
+app.appendChild(message)
+
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(movie => {
+      const a = document.createElement('a')
+      let link_title = movie.title.replace(regexp, '').toLowerCase()
+      if(movie.title === 'Castle in the Sky'){
+        a.href = link_base + 'laputa' + link_title
+      }else{
+        a.href = link_base + link_title
+      }
+
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0,300)
+      p.textContent = `${movie.description}...`
+
+      container.appendChild(card)
+
+      card.appendChild(a)
+      a.appendChild(h1)
+      a.appendChild(p)
+    })
+  })
+.catch(err => console.log(err))
